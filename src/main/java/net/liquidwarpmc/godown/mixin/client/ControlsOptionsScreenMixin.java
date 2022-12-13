acca875b251defe8eaf018850c20446a9923b1f2
Fran <franngoesa@gmail.com>
2022-12-13 14:35:01 -0300

    19.3

--- src/main/java/net/liquidwarpmc/godown/mixin/client/ControlsOptionsScreenMixin.java
+++ src/main/java/net/liquidwarpmc/godown/mixin/client/ControlsOptionsScreenMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -19,8 +20,14 @@ public class ControlsOptionsScreenMixin extends GameOptionsScreen {
         super(parent, gameOptions, title);
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/GameOptions;getAutoJump()Lnet/minecraft/client/option/SimpleOption;", shift = At.Shift.AFTER), method = "init", locals = LocalCapture.CAPTURE_FAILSOFT)
+    @Inject(method = "init", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/widget/ButtonWidget;builder(Lnet/minecraft/text/Text;Lnet/minecraft/client/gui/widget/ButtonWidget$PressAction;)Lnet/minecraft/client/gui/widget/ButtonWidget$Builder;"
+    ), slice = @Slice(
+            from = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/GameOptions;getOperatorItemsTab()Lnet/minecraft/client/option/SimpleOption;"),
+            to = @At(value = "RETURN")
+    ), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void init(CallbackInfo ci, int i, int j, int k) {
-        this.addDrawableChild(GoDownClient.crawlToggled.createButton(this.gameOptions, j, k, 150));
+        this.addDrawableChild(GoDownClient.crawlToggled.createButton(this.gameOptions, i, k + 24, 150));
     }
 }
