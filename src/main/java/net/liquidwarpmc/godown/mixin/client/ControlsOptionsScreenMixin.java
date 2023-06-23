851f58f8bf7fea73816315742abcc8732d712e48
Cookie <52550063+Covkie@users.noreply.github.com>
2023-06-23 04:08:57 -0400

    moves toggle to a pretty place
    fard

--- src/main/java/net/liquidwarpmc/godown/mixin/client/ControlsOptionsScreenMixin.java
+++ src/main/java/net/liquidwarpmc/godown/mixin/client/ControlsOptionsScreenMixin.java
@@ -1,33 +1,49 @@
 package net.liquidwarpmc.godown.mixin.client;
 
 import net.liquidwarpmc.godown.GoDownClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Slice;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(ControlsOptionsScreen.class)
-public class ControlsOptionsScreenMixin extends GameOptionsScreen {
+class ControlsOptionsScreenMixin extends GameOptionsScreen {
 
     public ControlsOptionsScreenMixin(Screen parent, GameOptions gameOptions, Text title) {
         super(parent, gameOptions, title);
     }
 
-    @Inject(method = "init", at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/widget/ButtonWidget;builder(Lnet/minecraft/text/Text;Lnet/minecraft/client/gui/widget/ButtonWidget$PressAction;)Lnet/minecraft/client/gui/widget/ButtonWidget$Builder;"
-    ), slice = @Slice(
-            from = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/GameOptions;getOperatorItemsTab()Lnet/minecraft/client/option/SimpleOption;"),
-            to = @At(value = "RETURN")
-    ), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void init(CallbackInfo ci, int i, int j, int k) {
-        this.addDrawableChild(GoDownClient.crawlToggled.createButton(this.gameOptions, i, k + 24, 150));
+    @ModifyArgs(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/SimpleOption;createWidget(Lnet/minecraft/client/option/GameOptions;III)Lnet/minecraft/client/gui/widget/ClickableWidget;", ordinal = 0))
+    void shrinkSneak(Args args){
+        args.set(3, 100);
+    }
+
+    @ModifyArgs(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/SimpleOption;createWidget(Lnet/minecraft/client/option/GameOptions;III)Lnet/minecraft/client/gui/widget/ClickableWidget;", ordinal = 1))
+    void shrinkSprint(Args args){
+        args.set(1, (int)args.get(1) - 55);
+        args.set(3, 100);
+    }
+
+    @Inject(
+            method = "init",
+            at = @At("TAIL")
+    )
+    void postInit(CallbackInfo ci) {
+        this.addDrawableChild(
+                GoDownClient.crawlToggled.createWidget(
+                        this.gameOptions,
+                        this.width / 2 - 155 + (105)*2,
+                        this.height / 6 - 12 + 24,
+                        100
+                )
+        );
     }
 }
